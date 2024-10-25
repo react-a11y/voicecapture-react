ead8ae4d8d477a6827e950e347713d7f26cac0a9
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-25 11:48:50 -0300

    adjustment example voice and improvement responsive, add icons in buttons

--- src/App.tsx
+++ src/App.tsx
@@ -35,39 +35,43 @@ function App() {
         onDeactivate={handleDeactivate}
       />
       <div className="content example">
-        <h2>Usage Example</h2>
+        <h2>VoiceCapture Example</h2>
 
-        <div className="example-actions">
-          <button
-            className="button-voicecapture-example"
-            onClick={() => openVoiceCapture("fullscreen")}
-          >
-            Fullscreen
-          </button>
+        <div className="content-actions">
+          <div className="example-actions">
+            <button
+              className="button-voicecapture-example"
+              onClick={() => openVoiceCapture("fullscreen")}
+            >
+              <span className="material-symbols-outlined"> &#xe029; </span>
+              <span className="material-symbols-outlined"> &#xe5d0; </span> FullScreen
+            </button>
 
-          <button
-            className="button-voicecapture-example"
-            onClick={() => openVoiceCapture("float")}
-          >
-            Float
-          </button>
-        </div>
+            <button
+              className="button-voicecapture-example"
+              onClick={() => openVoiceCapture("float")}
+            >
+              <span className="material-symbols-outlined"> &#xe029; </span>
+              <span className="material-symbols-outlined"> &#xe62e; </span> Float
+            </button>
+          </div>
 
-        <div className="language">
-          <label htmlFor="language-select" className="lang-label">
-            Select Language:
-          </label>
-          <select
-            id="language-select"
-            className="lang-select"
-            value={lang}
-            onChange={(e) => setLang(e.target.value)}
-          >
-            <option value="en">English</option>
-            <option value="pt">Portuguese</option>
-            <option value="es">Spanish</option>
-            <option value="fr">French</option>
-          </select>
+          <div className="language">
+            <label htmlFor="language-select" className="lang-label">
+              Select Language:
+            </label>
+            <select
+              id="language-select"
+              className="lang-select"
+              value={lang}
+              onChange={(e) => setLang(e.target.value)}
+            >
+              <option value="en">English</option>
+              <option value="pt">Portuguese</option>
+              <option value="es">Spanish</option>
+              <option value="fr">French</option>
+            </select>
+          </div>
         </div>
       </div>
 
