478f702d0fb786d45533a4cf7328fe2972ef9d93
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-25 14:40:27 -0300

    adjsutment desative voicecapture

--- src/VoiceCapture.tsx
+++ src/VoiceCapture.tsx
@@ -1,26 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 import "./VoiceCapture.scss";
 
 const translates: Record<string, Record<string, string>> = {
-  'en': {
-    speakNow: 'Speak now...',
-    noSpeech: 'No speech detected.',
-    audioCapture: 'Audio capture failed.',
-    enableMicrophone: 'Please enable your microphone.',
+  en: {
+    speakNow: "Speak now...",
+    noSpeech: "No speech detected.",
+    audioCapture: "Audio capture failed.",
+    enableMicrophone: "Please enable your microphone.",
   },
 };
 
-const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscript, onDeactivate }: any) => {
+const VoiceCapture = ({
+  start,
+  lang = "en",
+  mode = "fullscreen",
+  onVoiceTranscript,
+  onDeactivate,
+}: any) => {
   const [recognizing, setRecognizing] = useState(false);
   const [animationButton, setAnimationButton] = useState(false);
   const recognitionRef = useRef<any>(null);
   const textTipRef = useRef<HTMLParagraphElement | null>(null);
-  const finalTranscriptRef = useRef<string>('');
+  const finalTranscriptRef = useRef<string>("");
 
   useEffect(() => {
-    if ('webkitSpeechRecognition' in window) {
+    if ("webkitSpeechRecognition" in window) {
       const recognition = new (window as any).webkitSpeechRecognition();
       recognition.continuous = false;
       recognition.lang = lang;
@@ -28,12 +34,12 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
 
       recognition.onstart = () => {
         setRecognizing(true);
-        updateText('speakNow');
+        updateText("speakNow");
         setAnimationButton(true);
       };
 
       recognition.onerror = (event: any) => {
-        console.error('Recognition error:', event.error);
+        console.error("Recognition error:", event.error);
         setAnimationButton(false);
         handleError(event.error);
       };
@@ -42,11 +48,11 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
         setRecognizing(false);
         setAnimationButton(false);
         if (finalTranscriptRef.current) {
-          updateText('');
+          updateText("");
           onVoiceTranscript(finalTranscriptRef.current);
         } else {
-          console.warn('Recognition stopped without result.');
-          updateText('noSpeech');
+          console.warn("Recognition stopped without result.");
+          updateText("noSpeech");
         }
         setTimeout(deactivateVoice, 5000);
       };
@@ -55,7 +61,9 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
 
       recognitionRef.current = recognition;
     } else {
-      console.warn('SpeechRecognition not supported, please update your browser.');
+      console.warn(
+        "SpeechRecognition not supported, please update your browser."
+      );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lang, onVoiceTranscript]);
@@ -68,37 +76,35 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
   const activateVoice = () => {
     if (!recognizing && recognitionRef.current) {
       recognitionRef.current.lang = lang;
-      finalTranscriptRef.current = '';
+      finalTranscriptRef.current = "";
       setRecognizing(true);
       recognitionRef.current.start();
     }
   };
 
   const deactivateVoice = () => {
-    if (recognizing && recognitionRef.current) {
-      setRecognizing(false);
-      setAnimationButton(false);
-      recognitionRef.current.stop();
-      onDeactivate && onDeactivate(); // Notify parent component to deactivate `start`
-    }
+    setRecognizing(false);
+    setAnimationButton(false);
+    recognitionRef.current.stop();
+    onDeactivate && onDeactivate();
   };
 
   const handleError = (error: string) => {
-    console.warn('Handling error:', error);
+    console.warn("Handling error:", error);
     updateText(error);
 
     switch (error) {
-      case 'no-speech':
-        updateText('noSpeech');
+      case "no-speech":
+        updateText("noSpeech");
         break;
-      case 'audio-capture':
-        updateText('audioCapture');
+      case "audio-capture":
+        updateText("audioCapture");
         break;
-      case 'not-allowed':
-        updateText('enableMicrophone');
+      case "not-allowed":
+        updateText("enableMicrophone");
         break;
       default:
-        console.warn('Unknown error:', error);
+        console.warn("Unknown error:", error);
         break;
     }
 
@@ -106,7 +112,7 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
   };
 
   const handleResults = (event: any) => {
-    let interimTranscript = '';
+    let interimTranscript = "";
 
     for (let i = event.resultIndex; i < event.results.length; ++i) {
       if (event.results[i].isFinal) {
@@ -131,18 +137,29 @@ const VoiceCapture = ({ start, lang = 'en', mode = 'fullscreen', onVoiceTranscri
   };
 
   const getTranslation = (key: string): string => {
-    const translationsForLang = translates[lang] || translates['en'];
+    const translationsForLang = translates[lang] || translates["en"];
     return translationsForLang[key] || key;
   };
 
   return (
-    <section className={`voicecapture ${start ? 'active' : ''} ${mode ? mode : ''}`} onClick={deactivateVoice}>
-      <button className="exit" type="button" onClick={deactivateVoice}>
+    <section
+      className={`voicecapture ${start ? "active" : ""} ${mode ? mode : ""}`}
+      onClick={() => deactivateVoice()}
+    >
+      <button className="exit" type="button" onClick={() => deactivateVoice}>
         <i className="icon icon-exit">X</i>
       </button>
       <p ref={textTipRef} className="text-tip"></p>
-      <button type="button" className={`btn-voice ${animationButton ? 'active' : ''}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+      <button
+        type="button"
+        className={`btn-voice ${animationButton ? "active" : ""}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
           <path d="M17 11.998c0 2.76-2.23 5-4.99 5l-.002.002a4.994 4.994 0 01-4.979-5h-2c0 3.52 2.59 6.433 5.98 6.92v3.078h.01V22h2v-3.08h-.01A6.982 6.982 0 0019 11.998z" />
           <path fill="none" d="M0 0h24v24H0z" />
           <path d="M12 15c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v7c0 1.66 1.34 3 3 3z" />
